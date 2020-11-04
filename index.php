8cb5a3bb9811cedb1ddd6c2c28e4d51333f46895
Manojkumar <61974981+manojkumart06@users.noreply.github.com>
2020-11-04 20:07:22 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+ require_once 'header.php';
+ echo "<div class='center'>Welcome to Buddy Connect,";
+  if ($loggedin) echo " $user, you are logged in";
+    else         echo ' please sign up or log in';
+
+    echo <<<_END
+    </div><br>
+ </div>
+ <div data-role="footer">
+<marquee><h4>Thank you for visiting.</h4></marquee>
+ </div>
+ </body>
+</html>
+_END;
+?>
